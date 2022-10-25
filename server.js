a40107b9516ed5f4a7be6cdbd1b65322e9c32035
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2022-10-25 15:56:53 -0500

    Login User API endpoint

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import config from 'config';
 import User from './models/User';
+import auth from './middleware/auth';
 
 // Initialize express application
 const app = express();
@@ -30,7 +31,7 @@ app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
-app.get('/api', (req, res) => res.send('http get request sent to api'));
+app.get('/api/', (req, res) => res.send('http get request sent to api'));
 
 /**
  * @route POST api/users
@@ -47,7 +48,7 @@ app.post(
             'password',
             'Please enter a password with 6 or more characters'
         ).isLength({ min: 6 })
-    ], 
+    ],
     async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
@@ -56,7 +57,7 @@ app.post(
             const { name, email, password } = req.body;
             try {
                 // Check if user exists
-                let user = await User.findOne({ email: email});
+                let user = await User.findOne({ email: email });
                 if (user) {
                     return res
                         .status(400)
@@ -78,27 +79,86 @@ app.post(
                 await user.save();
 
                 // Generate and return a JWT token
-                const payload = {
-                    user: {
-                        id: user.id
-                    }
-                };
-
-                jwt.sign(
-                    payload,
-                    config.get('jwtSecret'),
-                    { expiresIn: '10hr' },
-                    (err, token) => {
-                        if (err) throw err;
-                        res.json({ token: token });
-                    }
-                );
+                returnToken(user, res);
             } catch (error) {
-                res.status(500).send('Server error ' + error);
+                res.status(500).send('Server error');
             }
         }
     }
 );
+
+/**
+ * @route GET api/auth
+ * @desc Authorize user
+ */
+app.get('/api/auth', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send('Unknown server error');
+    }
+});
+
+/**
+ * @route POST api/login
+ * @desc Login user
+ */
+app.post(
+    '/api/login',
+    [
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'A password is required').exists()
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({ errors: errors.array() });
+        } else {
+            const { email, password } = req.body;
+            try {
+                // Check if user exists
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+                // Check password
+                const match = await bcrypt.compare(password, user.password);
+                if (!match) {
+                    return res
+                        .status(400)
+                        .json({ errors: [{ msg: 'Invalid email or password' }] });
+                }
+
+                // Generate and return a JWT token
+                returnToken(user, res);
+            } catch (error) {
+                res.status(500).send('Server error');
+            }
+        }
+    }
+);
+
+const returnToken = (user, res) => {
+    const payload = {
+        user: {
+            id: user.id
+        }
+    };
+
+    jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: '10hr' },
+        (err, token) => {
+            if (err) throw err;
+            res.json({ token: token });
+        }
+    );
+};
 
 // Connection listener
 const port = 5000;
