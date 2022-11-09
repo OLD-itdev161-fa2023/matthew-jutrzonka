c35cba154365234a20471a4c16df77b559c4cafb
Matthew Jutrzonka <jutrzont@gmatc.matc.edu>
2022-11-09 17:52:52 -0600

    Create Endpoint to Delete Post

--- server.js
+++ server.js
@@ -217,7 +217,7 @@ app.get('/api/posts', auth, async(req, res) => {
         const posts = await Post.find().sort({ date: -1 });
 
         res.json(posts);
-    }   catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).send('Server error');
     }
@@ -237,6 +237,33 @@ app.get('/api/posts/:id', auth, async (req, res) => {
         }
 
         res.json(post);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
+
+/**
+ * @route DELETE api/posts/:id
+ * @desc Delete a post
+ */
+app.delete('/api/posts/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Make sure the post was found
+        if (!post) {
+            return res.status(404).json({ msg: 'Post not found' });
+        }
+
+        // Make sure the request user created the post
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({ msg: 'User not authorized' });
+        }
+
+        await post.remove();
+
+        res.json({ msg: 'Post removed' });
     } catch (error) {
         console.error(error);
         res.status(500).send('Server error');
